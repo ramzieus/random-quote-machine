5533c3ec47aa2ce3e7c7eea910971b5fbfb1d704
ramzi hemadou <ramzi.hemadou@flambosoft.com>
2019-10-24 15:32:36 +0100

    Api added and it's running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      
-      <header className="App-header">
-       <img 
-      
-        />
-       
-        <p id="App-link">
-        "Transformation does not start with <br />
-        some one else changing you;<br />
-         transformation is an inner self reworking of <br />
-         what you are now to what you will be."
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+        quote: 'Welcome on the random Quotes machine',
+        author: 'By Amina ...',
+    }
+    this.getQuote = this.getQuote.bind(this)
+}
+componentDidMount() {
+  setTimeout(this.getQuote(), 10000);
+}
+
+getQuote(){
+  return axios({
+    url: 'https://api.quotable.io/random',
+    method: 'GET'
+}).then(response => {
+    console.log(response.data);
+    this.setState({quote:response.data.content, author: response.data.author});
+    // setInterval(this.getQuote(), 3000);
+}).catch(error => {
+    alert('error no connection !')
+})
+}
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p id="App-link">
+            {this.state.quote}
+          </p>
+          <small>{this.state.author}</small>
+          <button onClick={() => this.getQuote()}>New</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
